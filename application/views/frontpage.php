60b674340fe8177dbaec0441709e76aaaf535b1b
Chris Mancuso <Christopher.a.mancuso@gmail.com>
2014-04-12 19:53:10 -0400

    First Commit

---
+++ application/views/frontpage.php
@@ -0,0 +1,65 @@
+<!-- Navbar, eventually migrate it to the main header -->
+<div class="row">
+	<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+	 <div class="container">
+	 		<div class = "navbar-header">
+	 			<button type = "button" class = "navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+	 				<span class="sr-only">Toggle navigation</span>
+	          	<span class="icon-bar"></span>
+	          	<span class="icon-bar"></span>
+	          	<span class="icon-bar"></span>
+	        </button>
+	        <a class ="navbar-brand" href="#">Napkinboards</a>
+	 		</div>
+	 		<div classs = "navbar-collapse">
+	 			<ul class ="nav navbar-nav">
+	 				<li class="active"><a href"#">Home</a></li>
+	 				<li><a href="#">Thing</a></li>
+	 				<li><a href="#">Thing2</a></li>
+	 			</ul>
+	   	</div>
+		</div>
+	</div>
+</div>
+<!-- main frame of the page -->
+<link href="assets/css/views/frontpage.css" type ="text/css" rel="stylesheet" />
+<div class="row">
+	<!-- Side bar -->
+	<div class="col-md-3">
+		sidebar
+	</div>
+	<!-- End sidebar -->
+	<!-- start main screen -->
+	<div class="col-md-9">
+		<h2>main thing</h2>
+			<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+	  		<!-- Indicators -->
+			  <ol class="carousel-indicators">
+			    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+			    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+			    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+			  </ol>
+
+	  <!-- Wrapper for slides -->
+	  			<div class="carousel-inner">
+	    			<div class="item active">
+	      			<img src="http://www.blogcdn.com/www.autoblog.com/media/2012/02/2013-ferrari-f12-berlinetta-lead.jpg" align="middle" alt="f12">
+	      				<div class="carousel-caption">
+	      				<h3>Black man</h3>
+					      </div>
+	    			</div>
+	    			<div class="item">
+	    				<img src="http://1.bp.blogspot.com/-v-sT2NHYhSk/UI0Zg-E4rVI/AAAAAAAAFPw/rGPCYkuqds8/s640/Lamborghini-Aventador-orange-Front-shot.jpg" align="middle" alt="aventador">
+	    			</div>
+	  			</div>
+
+	  <!-- Controls -->
+	  <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+	    <span class="glyphicon glyphicon-chevron-left"></span>
+	  </a>
+	  <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+	    <span class="glyphicon glyphicon-chevron-right"></span>
+	  </a>
+	</div>
+	</div>
+</div>
